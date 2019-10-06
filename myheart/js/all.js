7e8c86d84959d442cb27e9ea97440d49a50f6468
Butters <36911374+Butters0418@users.noreply.github.com>
2019-10-06 16:21:11 +0800

    new index

---
+++ myheart/js/all.js
@@ -0,0 +1,3 @@
+window.sr = ScrollReveal();
+sr.reveal('.s1_slogan', { interval: 250, reset: false, scale: 0.5, distance: '80px' });
+sr.reveal('.revealFade', { interval: 250, reset: false, distance: '80px' });
