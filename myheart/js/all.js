908f5630c35e5abcef8d7f77ac127fc941ee2010
Butters <36911374+Butters0418@users.noreply.github.com>
2019-10-14 21:00:31 +0800

    add section&video

--- myheart/js/all.js
+++ myheart/js/all.js
@@ -1,13 +1,14 @@
 document.getElementById('s13_bg').onload = function () {
   all();
 };
+
 function all() {
-  window.sr = ScrollReveal({ reset: false, delay: 200, viewFactor: 0.8 });
-  sr.reveal('.s1_slogan,.s5_title', { interval: 250, scale: 0.5, beforeReveal: rubberBand, });
-  sr.reveal('.revealFade', { interval: 250, distance: '80px' });
+  window.sr = ScrollReveal({ reset: false, viewFactor: 1 });
+  sr.reveal('.s1_slogan,.s5_title', { interval: 350, scale: 0.5, beforeReveal: rubberBand, });
+  sr.reveal('.revealFade', { interval: 300, distance: '80px', viewFactor: 0.2 });
   sr.reveal('.pop', { interval: 150, beforeReveal: bounceIn });
-  sr.reveal('.s13_ban', { interval: 100, delay: 400, afterReveal: bounce });
-  sr.reveal('.s13_qa', { interval: 100, delay: 0, viewFactor: 0.1 });
+  sr.reveal('.s13_ban', { interval: 300, delay: 400, afterReveal: bounce });
+  sr.reveal('.fadein', { interval: 300, delay: 500, viewFactor: 0.3 });
   function rubberBand(el) {
     $(el).addClass('rubberBand');
   }
@@ -32,14 +33,12 @@ function all() {
       clickable: true,
     },
   });
-
-  // [錨點]--// 判斷滑動位置
   $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
     var target = $(this).attr('href');
     var targetTop = $(target).offset().top - 50;
     $('html, body').stop().animate({
       scrollTop: targetTop
-    }, 300);
+    }, 400);
   });
 }
